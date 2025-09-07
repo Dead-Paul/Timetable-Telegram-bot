93241ba33810965002cc372160a6200ed8278521
Paul Dead <dead.every.side@gmail.com>
2025-09-07 14:11:16 +0200

    Module to work with json file
    JSON module gives possibility to get, set, values from the selected file in init

---
+++ JSON.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+class JSON:
+    def __init__(self, filename: str):
+        self.__filename = filename
+        if not os.path.exists(filename):
+            raise FileNotFoundError
+
+    def get(self, key: str) -> str | int | float | bool | dict | list | None:
+        with open(self.__filename, 'r', encoding="UTF-8") as json_file:
+            try:
+                return json.load(json_file)[key]
+            except KeyError:
+                return None
+
+    def set(self, values: dict[str, str | int | float | bool | dict | list | None]) -> None:
+        with open(self.__filename, 'r+', encoding="UTF-8") as json_file:
+            json_data = json.load(json_file)
+            assert isinstance(json_data, dict)
+            json_data.update(values)
+            json.dump(json_data, json_file, indent=2, ensure_ascii=False, allow_nan=False)
+
+        
