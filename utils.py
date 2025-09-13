0f22af7dde990d17123c50221acf4c26575998ed
Paul Dead <dead.every.side@gmail.com>
2025-09-13 14:24:34 +0200

    Created utils.py module
    Now contains Utils with distribution class that returns timedelta. This function was created for  distribution cycle in main telegram bot file

---
+++ utils.py
@@ -0,0 +1,62 @@
+from datetime import datetime, timedelta
+from typing import Any, Callable
+
+from modules.sql_queries import Queries, TableDicts
+from modules.timetable import Timetable, TimetableDicts
+
+
+class Utils:
+    def __init__(self, queries: Queries, timetable: Timetable):
+        self.queries = queries
+        self.timetable = timetable
+
+
+    def distribution(self, date_time: datetime, distribute: Callable[[str, list[str]], Any]) -> timedelta:
+        rings: list[TableDicts.RingDict] = self.timetable.get_rings(date_time.date())
+        weekday: TableDicts.WeekdayDict = self.queries.get_weekdays()[date_time.weekday()]
+
+        next_distribution: datetime|None = None
+        next_lesson: TimetableDicts.FoundLessonDict|None = None
+        date_time = date_time.replace(tzinfo=None)
+
+        if not weekday["is_work_day"] or rings[-1]["end"] < date_time:
+            next_distribution = rings[0]["start"] + timedelta(days=1) - timedelta(minutes=3)
+            for ring_id in range(1, len(rings)):
+                self.queries.clean_replacement_and_remind(weekday["id"], ring_id)
+        else:
+            if rings[0]["start"] - timedelta(minutes=3) > date_time:
+                next_distribution = rings[0]["start"] - timedelta(minutes=3)
+            else:
+                for ring_i in range(len(rings)):
+                    if abs((rings[ring_i]["start"] - timedelta(minutes=3)) - date_time) < timedelta(minutes=1):
+                        lesson: TimetableDicts.FoundLessonDict|str = self.timetable.find_lesson(date_time)
+                        if isinstance(lesson, str):
+                            next_lesson = self.timetable.find_next_lesson(weekday["id"], ring_i + 1, date_time)
+                            if next_lesson is not None and next_lesson["lesson"] is not None:
+                                distribute(f"Далі буде {next_lesson['lesson']['name']}\n<i>В {next_lesson['ring']['start']}.</i>", ["study", "sad"])
+                                next_distribution = next_lesson["ring"]["start"] - timedelta(minutes=3)
+                                break
+                            next_distribution = rings[0]["start"] + timedelta(days=1) - timedelta(minutes=3)
+                            break
+                        elif isinstance(lesson["lesson"], dict) and lesson["lesson"]["lesson_id"] != 1:
+                            distribute(
+                                f"{lesson['lesson']['name']} {lesson['lesson']['link']}" + (lesson["lesson"]["remind"] or ""), 
+                                ["study", "sad"]
+                            )
+                            next_distribution = rings[ring_i]["end"]
+                            break
+                    elif abs(rings[ring_i]["end"] - date_time) < timedelta(minutes=1):
+                        self.queries.clean_replacement_and_remind(weekday["id"], ring_i + 1)
+                        next_lesson = self.timetable.find_next_lesson(weekday["id"], ring_i + 1, date_time)
+                        if next_lesson is not None and next_lesson["lesson"] is not None:
+                            distribute(f"Далі буде {next_lesson['lesson']['name']}\n<i>В {next_lesson['ring']['start']}.</i>", ["study", "sad"])
+                            next_distribution = next_lesson["ring"]["start"] - timedelta(minutes=3)
+                            break
+                        next_distribution = rings[0]["start"] + timedelta(days=1) - timedelta(minutes=3)
+                        distribute(f"На сьогодні зайняття закінчились!", ["happy", "lovely"])
+                        break
+
+        if isinstance(next_distribution, datetime):
+            return next_distribution - date_time
+        else:
+            return timedelta(seconds=30)
