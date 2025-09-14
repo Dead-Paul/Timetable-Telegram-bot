68c66e627042c2122a03b61533cbab025133ec62
Paul Dead <dead.every.side@gmail.com>
2025-09-14 14:27:39 +0200

    Fixed overwriting and reading
    Now overwrites json file correctly and reads more simple, without excepting KeyError (which may be returned later for logging non existent keys)

--- modules/json_file.py
+++ modules/json_file.py
@@ -1,5 +1,6 @@
-import json
 import os
+import json
+from typing import Any
 
 class JSON_File:
     def __init__(self, filename: str):
@@ -7,19 +8,18 @@ class JSON_File:
         if not os.path.exists(filename):
             raise FileNotFoundError
 
-    def get(self, key: str) -> str|int|float|bool|dict|list|None:
+    def get(self, key: str) -> Any:
         with open(self.__filename, 'r', encoding="UTF-8") as json_file:
-            try:
-                return json.load(json_file)[key]
-            except KeyError:
-                return None
+            return json.load(json_file).get(key)
 
-    def set(self, values: dict[str, str|int|float|bool|dict|list|None]) -> None:
+    def set(self, values: dict[str, Any]) -> None:
         with open(self.__filename, 'r+', encoding="UTF-8") as json_file:
             json_data = json.load(json_file)
             assert isinstance(json_data, dict)
             json_data.update(values)
+            json_file.seek(0)
             json.dump(json_data, json_file, indent=2, ensure_ascii=False, allow_nan=False)
+            json_file.truncate()
 
 if __name__ == "__main__":
     exit()
