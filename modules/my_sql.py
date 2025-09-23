f3a802bf8f975febf96f396a331a9ae2a2a15086
Paul Dead <dead.every.side@gmail.com>
2025-09-24 00:08:48 +0200

    Added wrapper to prevent connection disconnect
    Updated MySql class, added connect, cursor, close functions

--- modules/my_sql.py
+++ modules/my_sql.py
@@ -4,14 +4,41 @@ from typing import cast
 
 import mysql.connector.cursor
 
+from .dict_types import MySQLConnectionDict
+
 class MySQL:
-    def __init__(self, user: str, password: str, host: str, database: str, logger: logging.Logger, autocommit: bool = False):
+    def __init__(self, connection_dict: MySQLConnectionDict, logger: logging.Logger, autocommit: bool = False):
+        self.connection_dict: MySQLConnectionDict = connection_dict
+        self.logger: logging.Logger = logger
+        self.__connection: mysql.connector.MySQLConnection|None = None
+        self.__cursor: mysql.connector.cursor.MySQLCursorDict|None = None
+        self.connect()
+
+    def connect(self) -> None:
         try:
-            self.connection = mysql.connector.connect(user=user, password=password, host=host, database=database, autocommit=autocommit)
-            self.cursor = cast(mysql.connector.cursor.MySQLCursorDict, self.connection.cursor(dictionary=True))
+            self.__connection = cast(mysql.connector.MySQLConnection, mysql.connector.connect(**self.connection_dict))
         except mysql.connector.Error as error:
-            logger.error(f"Database connection error: \"{error.msg}\"")
+            self.logger.error(f"Database connection error: \"{error.msg}\"")
             raise
+
+    def cursor(self) -> mysql.connector.cursor.MySQLCursorDict:
+        if self.__connection is None or not self.__connection.is_connected():
+            self.connect()
+            self.__cursor = None
+        assert self.__connection is not None
+        if self.__cursor is None:
+            self.__cursor = cast(mysql.connector.cursor.MySQLCursorDict, self.__connection.cursor(dictionary=True))
+        return self.__cursor
+
+    def close(self) -> bool:
+        if self.__connection is not None and self.__connection.is_connected():
+            if self.__cursor is not None:
+                self.__cursor.close()
+                self.__cursor = None
+            self.__connection.close()
+            self.__connection = None
+            return True
+        return False
 
 if __name__ == "__main__":
     exit()
